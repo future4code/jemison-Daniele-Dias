0cbc776785e3a8a78699e6d8f93d3f8c826c2896
Daniele Dias <professoradanidias@gmail.com>
2022-04-15 01:41:55 -0300

    Exercício sobre variáveis

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,1 +1,49 @@
-fdfdfd
+
+//Exercício de escrita de código
+
+//1. Consrua um programa, seguindo os passos:
+
+//a) Declare uma variável para armazenar um nome, sem atribuir um valor.
+
+//let primeiroNome
+
+
+
+//b) Declare uma variável para armazenar uma idade, sem atribuir um valor.
+
+// let idade
+
+
+//c) Imprima na tela o tipo dessas variáveis que acabou de criar, usando o comando typeof.
+
+//console.log(typeof primeiroNome)
+
+
+// d) Reflita: por que esse tipo foi impresso? Escreva a resposta em um comentário de código.
+
+// Foi impresso pois o primeiro nome é indefinido, ou seja, não lhe foi atribuído nenhum valor.
+
+
+//e) Pergunte ao usuário seu nome e sua idade, atribuindo esses dois valores às variáveis que acabou de criar.
+
+// let nome = prompt ("Qual é o seu nome?");
+
+// console.log (nome)
+
+//let idade = prompt ("Qual é a sua idade?");
+
+//console.logo (idade)
+
+
+//f) Novamente, imprima na tela o tipo dessas variáveis. O que você notou? Escreva em um comentário de código.
+
+// console.log = (typeof nome)
+
+
+//g) Para finalizar, imprima na tela a mensagem: "Olá nome, você tem idade anos". Onde nome e idade são os valores que o usuário inseriu.
+
+let nome = prompt ("Qual o se nome?")
+let idade = prompt ("Qual a sua idade?")
+
+console.log ("Olá" , nome, "você tem" , idade , "anos")
+
