acf7961ba2a708e452ebfd40e41bd31f0b46462b
Daniele Dias <professoradanidias@gmail.com>
2022-04-21 03:44:54 -0300

    Conclusão do exercício operadores

---
+++ operadores/index.js
@@ -0,0 +1,92 @@
+    // 1) Leia o código
+
+//const bool1 = true
+//const bool2 = false
+//const bool3 = !bool2
+
+//let resultado = bool1 && bool2
+//console.log("a. ", resultado)
+//false
+
+// resultado = bool1 && bool2 && bool3 
+// console.log("b. ", resultado)
+//false
+
+// resultado = !resultado && (bool1 || bool2) 
+// console.log("c. ", resultado)
+//true
+
+// console.log("d. ", typeof resultado)
+//boolean
+
+
+
+    // 2) Seu colega se aproxima de você falando...
+
+//let primeiroNumero = prompt("Digite um numero!")
+//let segundoNumero = prompt("Digite outro numero!")
+
+//const soma = primeiroNumero + segundoNumero
+
+//console.log(soma)
+// Seria impresso no console a junção do primeiro e do segundo numero digitados e não a soma.
+
+
+
+    // 3) Uma sugestão para que o resultado seja a soma dos dois números:
+
+//const primeiroNumero = prompt("Digite um numero!")
+//const segundoNumero = prompt("Digite outro numero!")
+//const resultado = primeiroNumero + segundoNumero
+//console.log(resultado)
+
+
+
+
+    //EXERCÍCIO DE ESCRITA DE CÓDIGO
+
+    //1)
+
+// const suaIdade = prompt ("Qual a sua idade?")
+// const idadeAmigo = prompt ("Qual a idade do seu melhor amigo?")
+
+
+// console.log("Sua idade é maior do que a do seu melhor amigo?" , suaIdade>idadeAmigo)
+
+
+    //2) 
+
+// const numeroPar = prompt("Por favor, insira um número par.")
+
+// const restoDaDivisao = numeroPar %2
+
+// console.log(restoDaDivisao)
+
+
+    //3) ...pergunte ao usuário sua idade em anos. Depois imprima no console
+
+// const idade = prompt("Qual a sua idade?")
+// const idadeMeses = (idade*12)
+// const idadeDias = (idade*365)
+// const idadeHoras = (idade*365)*24
+
+// console.log(idadeMeses)
+// console.log (idadeDias)
+// console.log (idadeHoras)
+
+
+    // 4) 
+
+const primeiroNumero = Number(prompt("Por favor, digite um número."))
+const segundoNumero = Number(prompt("Por favor, digite outro número agora."))
+
+
+console.log("O primeiro número é maior que o segundo número?" , primeiroNumero>segundoNumero)
+console.log("O primeiro número é igual ao segundo?" , primeiroNumero===segundoNumero)
+console.log("O primeiro número é divisível pelo segundo?" , primeiroNumero%segundoNumero===0)
+console.log("O segundo número é divisível pelo primeiro?" , segundoNumero%primeiroNumero===0)
+
+
+
+
+
