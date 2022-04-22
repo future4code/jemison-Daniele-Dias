cf65a79bb4eb79315dbc17666f15d61818cc6c33
Daniele Dias <professoradanidias@gmail.com>
2022-04-22 18:34:02 -0300

    Exercícios sobre funções EM ANDAMENTO

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,60 @@
+//  1) Leia o codigo
+
+// function minhaFuncao(variavel) {
+// 	return variavel * 5
+// }
+
+// console.log(minhaFuncao(2))
+// console.log(minhaFuncao(10))
+
+// a) O que vai ser impresso no console?
+// 10 - multiplicando 5 por 2
+// 50 - multiplicando 5 por 10
+
+
+// b) O que aconteceria se retirasse os dois console.log e simplemente invocasse a função...
+//  Não ia imprimir nada no console.
+
+
+// 2) Leia o código abaixo
+
+// a)
+// A função está mandando incluir o texto toda em maiúsculas e incluir a palavra cenoura.
+// No console vai aparecer cenoura e o texto que o usuário colocou.
+
+
+
+// EXERCÍCIO ESCRITA DE CÓDIGO
+
+// 1)
+// a) A função não deve receber nenhum parâmetro e deve imprimir uma mensagem falando algumas informações sobre você, como: 
+
+// const nome = "Dani"
+// const idade = 44
+// const endereco = "Rua Golfo de Cadis, 206. Apto 303. Intermares. PB"
+// const profissao = "pedagoga"
+
+// console.log("Eu sou" , nome , "tenho" , idade , "anos, moro na" , endereco , "e sou" , profissao)
+
+
+
+const nome = prompt ("O meu nome é ")
+const idade = prompt ("A minha idade é ")
+const endereco = prompt ("O meu endereço é ")
+const profissao = prompt ("A minha profissão é ")
+
+//  Ainda não sei como transformar em função. 
+
+
+// 2) Escreva as funções explicadas abaixo:
+// a)
+
+
+ESTUDO EM ANDAMENTO!
+
+
+
+
+
+
+
